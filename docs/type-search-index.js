c55d8c5e04b3ab32e8bf110081aba2e9f2567706
Victor Ferreira <125702186+VictorDsFerreira@users.noreply.github.com>
2025-11-25 21:42:12 -0300

    Commit JavaDoc for Calculadora.java

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"Calculadora","l":"Calculadora"},{"p":"Calculadora","l":"TesteCalculadora"}];updateSearchResults();
